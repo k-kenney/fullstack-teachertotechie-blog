782b78e40dadb9b00364a436468981db56affe0f
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-06-19 07:25:17 -0500

    first commit

---
+++ teachertotechie-frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesListPage from "./pages/ArticlesListPage";
+import HomePage from "./pages/HomePage";
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <h1>My Awesome Blog</h1>
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/articles" element={<ArticlesListPage />} />
+            <Route path="/articles/:articleId" element={<ArticlePage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
